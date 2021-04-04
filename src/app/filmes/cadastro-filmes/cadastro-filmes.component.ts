abec914e2ee6ca7875c5ea13d740562905a3216b
Thales Bento <59827737+webeleven-tbarbosa@users.noreply.github.com>
2021-04-03 22:14:03 -0300

    fomulario de cadastro de filmes

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'dio-cadastro-filmes',
@@ -8,17 +8,33 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 })
 export class CadastroFilmesComponent implements OnInit {
 
-  options: FormGroup;
+  cadastro: FormGroup;
 
   constructor(private fb: FormBuilder) { }
 
-  ngOnInit() {
-
-    this.options = this.fb.group({
-      hideRequired: false,
-      floatLabel: 'auto',
-    });
-
+  get f(){
+    return this.cadastro.controls;
   }
 
+  ngOnInit(): void {
+    this.cadastro = this.fb.group({
+      titulo: ['',[Validators.required,Validators.minLength(5),Validators.maxLength(100)]],
+      urlFoto: ['',[Validators.minLength(10)]],
+      dtLancamento: ['',[Validators.required]],
+      descricao: [''],
+      nota: [0,[Validators.required,Validators.min(0),Validators.max(10)]],
+      urlImdb: ['',[Validators.minLength(10)]],
+      genero: ['',[Validators.required]],
+    });
+  }
+  salvar() : void {
+    this.cadastro.markAllAsTouched();
+    if(this.cadastro.invalid){
+      return;
+    }
+    alert('Success!!\n\n' + JSON.stringify(this.cadastro.value, null, 4));
+  }
+  resetarForm() : void {
+    this.cadastro.reset();
+  }
 }
